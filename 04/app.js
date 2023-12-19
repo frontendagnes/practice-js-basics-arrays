6d0fa6befdcc909ffa20d610e9fa96c62c1fa7a9
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-19 14:09:25 +0100

    fourth

--- 04/app.js
+++ 04/app.js
@@ -1,1 +1,12 @@
 const years = [1980, 1934, 2002, 2019];
+const passedYears = [];
+
+function howYears(year) {
+  years.map((item) => {
+    let el = year - item;
+    passedYears.push(el);
+  });
+  console.log(passedYears);
+}
+
+howYears(2023);
