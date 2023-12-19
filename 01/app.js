c3848671d29047a8f44fedeb1b84d0f25be31c43
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-19 13:21:05 +0100

    frist

---
+++ 01/app.js
@@ -0,0 +1,13 @@
+const users = [
+  "Adam Nowak",
+  "Jan Kowalski",
+  "Monika Oz",
+  "Deamon Salvatore",
+  "Stefan Salvatore",
+  "Elena Gilbert",
+  "Bonnie Benet",
+];
+
+const usersLength = users.length;
+
+console.log("Lenght", usersLength);
